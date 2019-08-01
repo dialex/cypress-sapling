82eb78ccd3a7cda7a666a2fc63fb0afcb4611f59
Diogo Nunes <dialex.nunes@gmail.com>
2019-08-01 15:40:05 +0100

    test: 🔍 include test template/example

---
+++ cypress/integration/template.spec.js
@@ -0,0 +1,11 @@
+// EXAMPLE
+
+describe("API name", function() {
+  context("/endpoint", function() {
+    it("expects a certain behaviour", function() {
+      cy.request("https://www.google.com/")
+        .its("status")
+        .should("equal", 200)
+    })
+  })
+})
